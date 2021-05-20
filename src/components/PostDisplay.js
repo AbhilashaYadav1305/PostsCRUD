0067249006848e844d5f4ee2e8e5d77ebb4b6208
AbhilashaYdav1305 <AbhilashaYdav1305>
2021-05-20 18:39:21 +0530

    searching Functionality completed

--- src/components/PostDisplay.js
+++ src/components/PostDisplay.js
@@ -6,7 +6,8 @@ const PostDisplay = (props) => {
 
   const [data, setData] = useState([])
   const [filteredData, setFilteredData] = useState([])
-  const [searchKeyword, setSearchKeyword] = useState([])
+  const [fetchedData, setfetchedData] = useState([])
+  const [searchKeyword, setSearchKeyword] = useState("")
   const header =
   {
     "userId": "User Id",
@@ -25,14 +26,22 @@ const PostDisplay = (props) => {
 
   }
 
-  const handleSearch = (searchKey) => {
-    let tempData = data.map((dataObj, i) => {
-      let isfound = Object.entries(dataObj).filter((obj) => obj.id === searchKey)
-      console.log(isfound)
-    })
+  const handleSearch = (e) => {
+    console.log(searchKeyword)
+    if (searchKeyword !== "") {
+      let tempData = [];
+      data.map((dataObj, i) => {
+        let flag = false;
+        let isfound = Object.keys(header).filter((head) => (dataObj[head].toString().includes(searchKeyword.toString())))
+        if (isfound.length > 0) return tempData.push(dataObj)
+      })
+      console.log(tempData)
+      setData(tempData);
+    } else setData(fetchedData);
+  }
 
-    console.log(tempData)
-    // setFilteredData(tempData);
+  const handleChange = (e) => {
+    setSearchKeyword(e.target.value)
   }
 
   useEffect(async () => {
@@ -43,45 +52,34 @@ const PostDisplay = (props) => {
     }
 
     const data = await axios.get('https://jsonplaceholder.typicode.com/posts', config);
-    setData(data.data);
+    setfetchedData(data.data);
+    setData(data.data)
   }, [])
+
+  // useEffect(async () => {
+  //   if (filteredData.length !== 0)
+  //     setData(filteredData);
+  //   else
+  //     setData(fetchedData);
+  // }, [filteredData])
 
   return (
     <div>
-      <input type="text"></input><i class="fas fa-search" onClick={handleSearch}>Search</i>
+      <input type="text" onChange={handleChange} value={searchKeyword}></input><i class="fas fa-search" onClick={handleSearch}>Search</i>
       <table style={{ border: "1px solid black", width: "80%", marginLeft: "20px", marginTop: "20px", marginBottom: "50px" }}>
         <tr>
           {Object.entries(header).map((value) => <th style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}>{value[1]}</th>)}
         </tr>
-        {/* {Object.entries(header).map((head) => {
-          data.map((dataObj) => {
-         
-          })
-            < td ></td>})
-        } */}
         {
-          filteredData.length > 0 ? filteredData.map((dataObj) => {
+          data.map((dataObj) => {
             return <tr>
               {Object.keys(header).map((headKey) => <td style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}>{dataObj[headKey]}</td>)}
               <td style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}><i class="fas fa-edit" onClick={handleEdit}>Edit</i></td>
               <td style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}><i class="fas fa-trash-alt" onClick={handleDelete}>Delete</i></td>
             </tr>
-          }) :
-            data.map((dataObj) => {
-              return <tr>
-                {Object.keys(header).map((headKey) => <td style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}>{dataObj[headKey]}</td>)}
-                <td style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}><i class="fas fa-edit" onClick={handleEdit}>Edit</i></td>
-                <td style={{ borderRight: "1px solid black", borderBottom: "1px solid black" }}><i class="fas fa-trash-alt" onClick={handleDelete}>Delete</i></td>
-              </tr>
-            })
-        }
-        {/* {
-          Object.keys(header).map((headKey) => {
-            data.map(() =>{
-
-            })
           })
-        } */}
+        }
+
       </table>
     </div>
   )
